fa2f3129e8f7e83df71fd6bbe8e4b09034f7fc66
diego123732 <diegocrack345678@gmail.com>
2026-03-09 17:56:42 +0100

    I added more tests, the news are for the auth of the employees.

--- backend_restaurantmanager/src/test/java/com/tfg_rm/backend_restaurantmanager/controller/AuthControllerTest.java
+++ backend_restaurantmanager/src/test/java/com/tfg_rm/backend_restaurantmanager/controller/AuthControllerTest.java
@@ -3,6 +3,7 @@ package com.tfg_rm.backend_restaurantmanager.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tfg_rm.backend_restaurantmanager.auth.controller.AuthController;
 import com.tfg_rm.backend_restaurantmanager.auth.dto.ClientLoginRequest;
+import com.tfg_rm.backend_restaurantmanager.auth.dto.EmployeeLoginRequest;
 import com.tfg_rm.backend_restaurantmanager.shared.security.JwtService;
 
 import jakarta.servlet.ServletException;
@@ -47,6 +48,34 @@ class AuthControllerTest {
 
         assertThrows(ServletException.class, () -> {
             mockMvc.perform(post("/auth/clientLogin")
+                    .contentType("application/json")
+                    .content(mapper.writeValueAsString(request)));
+        });
+    }
+
+    @Test
+    void employeeShouldLoginSuccessfully() throws Exception {
+        EmployeeLoginRequest request = new EmployeeLoginRequest();
+        request.setDni("12345678");
+        request.setPassword("1234");
+
+        mockMvc.perform(post("/auth/employeeLogin")
+                .contentType("application/json")
+                .content(mapper.writeValueAsString(request)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.employeeId").exists())
+                .andExpect(jsonPath("$.restaurantIds").exists());
+    }
+
+    @Test
+    void employeeShouldFailLogin() throws Exception {
+
+        EmployeeLoginRequest request = new EmployeeLoginRequest();
+        request.setDni("222");
+        request.setPassword("1020");
+
+        assertThrows(ServletException.class, () -> {
+            mockMvc.perform(post("/auth/employeeLogin")
                     .contentType("application/json")
                     .content(mapper.writeValueAsString(request)));
         });
